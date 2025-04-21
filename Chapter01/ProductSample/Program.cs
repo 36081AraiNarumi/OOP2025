a978375d6eb6470ed0945a3f7354f02ecc67fbe7
infosys <infosys@C4234>
2025-04-21 09:57:49 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter01/ProductSample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ProductSample{
+    internal class Program{
+        static void Main(string[] args){
+
+           Product karinto = new Product(123, "かりんとう", 180);
+            Product daifuku = new Product(123, "だいふこ", 325);
+
+
+
+            //税抜きの価格を表示【かりんとうの税抜き価格は○○円です】
+
+            Console.WriteLine(karinto.Name + "の税抜き価格は" + karinto.Price + "です。");
+
+            //消費税額の表示【かりんとうの消費税額は○○円です】
+            Console.WriteLine(karinto.Name + "の消費税額は" + karinto.GetTax() + "です。");
+            //税込みの表示【かりんとうのぜいこみ価格は○○円です】
+            Console.WriteLine(karinto.Name + "の税込み価格は" + karinto.GetPriceIncludingTax() + "円です");
+
+           // 税抜きの価格を表示【かりんとうの税抜き価格は○○円です】
+
+            Console.WriteLine(daifuku.Name + "の税抜き価格は" + daifuku.Price + "です。");
+
+            //消費税額の表示【かりんとうの消費税額は○○円です】
+            Console.WriteLine(daifuku.Name + "の消費税額は" + daifuku.GetTax() + "です。");
+            //税込みの表示【かりんとうのぜいこみ価格は○○円です】
+            Console.WriteLine(daifuku.Name + "の税込み価格は" + daifuku.GetPriceIncludingTax() + "円です");
+        }
+    }
+}
