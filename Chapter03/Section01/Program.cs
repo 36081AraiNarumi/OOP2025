4e8977e4a096977bc84f4b8d2d5a8e5d32db1ea4
AraiNarumi <oibc36081@ojs.ac.jp>
2025-05-14 11:42:46 +0900

    コード3.1完成

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("カウントしたい数値：");
+            int input = int.Parse(Console.ReadLine());
+
+            int result = Count(input);
+
+            Console.WriteLine($"{input} は {result} 回出現します。");
+
+
+
+
+        }
+        static int Count(int num) {
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var count = 0;
+            foreach (var n in numbers) {
+                if(n == num) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
