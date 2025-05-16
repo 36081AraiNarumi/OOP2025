d51ce2dee6cd7cac631332cc66336c970ba04d13
AraiNarumi <oibc36081@ojs.ac.jp>
2025-05-16 15:58:43 +0900

    完成

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -4,34 +4,22 @@ namespace Section01 {
     internal class Program {
         public delegate bool Judgement(int value); //
         static void Main(string[] args) {
+            var cities = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
 
-           
+            var lowerList = cities.ConvertAll(s => s.ToUpper());
+            lowerList.ForEach(s => Console.WriteLine(s));
 
-
-          //  Console.WriteLine("カウントしたい数値：");
-           // int num = int.Parse(Console.ReadLine());
-
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-
-          //  Judgement judge = IsEven;
-            Console.WriteLine(Count( numbers, n=> 5 <= n && n<10));
         }
 
-        //メソッドへ渡す処理
-        // static bool IsEven(int n) {
-        //   return n % 2 == 1;
 
-        //  }
-        static int Count(int[] numbers, Predicate<int> judge) {
-            
-            var count = 0;
-            //引数で受け取ったメソッドを呼び出す
-            foreach (var n in numbers) {
-                if(judge(n) == true) {
-                    count++;
-                }
-            }
-            return count;
-        }
     }
 }
