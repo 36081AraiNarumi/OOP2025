970b95a24bbcd0bd9f2ad5fbdd86b2b8736e0367
AraiNarumi <oibc36081@ojs.ac.jp>
2025-06-11 11:21:33 +0900

    できました

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -1,38 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercise01 {
-    public class YearMonth {
-        //5.1.1
-        public int Year { get; set; }
-        public int Month { get; set; }
-        public YearMonth(int Year, int Month) {
-            this.Year = Year;
-            this.Month = Month;   
-        }
-        //5.1.2
-        //設定されている西暦が21世紀か判断する
-        //Yearが2001～2100年の間ならtrue,それ以外ならfalseを返す
-        public bool Is21Century {
-            get {
-                return Year >= 2001 && Year <= 2100;
-            }
-        }
-        //4.1.3
-        public YearMonth AddOneMonth() {
-            if (Mouth == 12) {
-                return new YearMonth(Year, )
+    // 5.1.1
+    public record YearMonth(int Year, int Month) {
 
+        // 5.1.2
+        //設定されている西暦が21世紀か判定する
+        //Yearが2001～2100年の間ならtrue、それ以外ならfalseを返す
+        public bool Is21Century => 2001 <= Year && Year <= 2100;
+
+        // 5.1.3
+        public YearMonth AddOneMonth() {
+            if (Month <= 11) {
+                return new YearMonth(Year, Month + 1); //Monthが12月以外
+            } else {
+                return new YearMonth(Year + 1, 1);//Monthが12月
             }
+
         }
-        //4
-        //.1.4
-        public override string ToString() =>
-            
-        
+
+        // 5.1.4
+        public override string ToString() => $"{Year}年{Month}月";
     }
+
 }
