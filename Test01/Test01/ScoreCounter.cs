a3553ce20f4e52b7fb51aeda442088661282c3aa
AraiNarumi <oibc36081@ojs.ac.jp>
2025-06-17 11:56:24 +0900

    設問１_コメント入力

--- Test01/Test01/ScoreCounter.cs
+++ Test01/Test01/ScoreCounter.cs
@@ -11,21 +11,30 @@
         private static IEnumerable<Student> ReadScore(string filePath) {
             var sales = new List<Student>();
             var lines = File.ReadAllLines(filePath);
-            foreach(var line in lines) {
+            foreach (var line in lines) {
                 string[] items = line.Split(',');
-                
+
                 Student student = new Student() {
                     Name = items[0],
                     Subject = items[1],
                     Score = int.Parse(items[2])
                 };
                 sales.Add(student);
-
-
             }
+            return sales;
+        }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
+            var dict = new SortedDictionary<string, int>();
+            foreach (var student in _score) {
+                if (dict.ContainsKey(student.Name)) {
+                    dict[student.Name] += student.Score;
+                } else {
+                    dict[student.Name] = student.Score;
+                }
+            }
+            return dict;
 
 
 
