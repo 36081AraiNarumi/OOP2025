43826b0780d95a773fedcd52a70884bee26c6144
AraiNarumi <oibc36081@ojs.ac.jp>
2025-07-15 15:29:40 +0900

    2完成

---
+++ Chapter12/演習問題/Exercise02/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+using System.IO;
+
+using System.Text.Json;
+
+using System.Text.Json.Serialization;
+
+
+
+namespace Exercise02 {
+
+    internal class Program {
+
+        static void Main(string[] args) {
+
+            var jsonString = File.ReadAllText("novelist.json");
+
+            var novelist = Deserialize(jsonString);
+
+            if (novelist is not null) {
+
+                Console.WriteLine(novelist);
+
+                foreach (var item in novelist.Masterpieces) {
+
+                    Console.WriteLine(item);
+
+                }
+
+            }
+
+        }
+
+        static Novelist? Deserialize(string jsonString) {
+
+            var opitions = new JsonSerializerOptions {
+
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+
+                NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals |
+
+                                 JsonNumberHandling.AllowReadingFromString
+
+            };
+
+            var novelist = JsonSerializer.Deserialize<Novelist>(jsonString, opitions);
+
+            return novelist;
+
+        }
+
+    }
+
+    public record Novelist {
+
+        public int Id { get; init; }
+
+        public string Name { get; init; } = string.Empty;
+
+        [JsonPropertyName("birth")]
+
+        public DateTime Birthday { get; init; }
+
+        public string[] Masterpieces { get; init; } = [];
+
+    }
+
+}
+
