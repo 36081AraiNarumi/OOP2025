4d48b7a8ed1b639e758b630882d796771f8fe4e3
AraiNarumi <oibc36081@ojs.ac.jp>
2025-10-08 10:58:34 +0900

    sanple

---
+++ WPF/PrismObservesSample/ViewModel.cs
@@ -0,0 +1,65 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+
+namespace PrismObservesSample {
+    class MainWindowViewModel : BindableBase {
+
+        private string _input1 = "";
+
+        public string Input1 {
+
+            get => _input1;
+
+            set => SetProperty(ref _input1, value);
+
+        }
+
+        private string _input2 = "";
+
+        public string Input2 {
+
+            get => _input2;
+
+            set => SetProperty(ref _input2, value);
+
+        }
+
+        private string _result = "";
+
+        public string Result {
+
+            get => _result;
+
+            set => SetProperty(ref _result, value);
+
+        }
+
+        // コンストラクタ
+
+        public MainWindowViewModel() {
+
+            SumCommand = new DelegateCommand(ExcuteSum, canExcuteSum);
+
+        }
+
+        public DelegateCommand SumCommand { get; }
+
+        // 足し算の処理
+
+        private void ExcuteSum() {
+
+            Result =  ;
+
+        }
+
+        //足し算処理を実行できるか否かのチェック
+
+        private bool canExcuteSum() {
+
+        }
+
+    }
+
+}
+
---
+++ WPF/SampleUnitConverter/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleUnitConverter {
+    internal class MainWindowViewModel:ViewModel{
+
+        //フィールド
+        private double metricValue;
+        private double imperiaValue;
+
+        public double MetricValue {
+            get => metricValue;
+            set {
+                this.metricValue = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged() {
+            
+        }
+
+        public double ImperialValue {
+            get => imperiaValue;
+            set {
+                this.imperiaValue = value;
+            }
+        }
+
+        //プロパティ
+    }
+}
