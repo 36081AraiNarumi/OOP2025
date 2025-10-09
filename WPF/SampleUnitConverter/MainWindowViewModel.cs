b0dcad571e23858b5e6358c2a01659cd25609696
AraiNarumi <oibc36081@ojs.ac.jp>
2025-10-09 14:28:34 +0900

    単位変換アプリ完成プリズム変更まで

--- WPF/SampleUnitConverter/MainWindowViewModel.cs
+++ WPF/SampleUnitConverter/MainWindowViewModel.cs
@@ -1,22 +1,15 @@
 ﻿using System;
-
 using System.Collections.Generic;
-
 using System.Linq;
-
 using System.Text;
-
 using System.Threading.Tasks;
-
 using System.Windows.Input;
 
 namespace SampleUnitConverter {
-
     internal class MainWindowViewModel : ViewModel {
 
         //フィールド
-
-        private double metricValue;
+       private double metricValue;
         private double imperialValue;
 
         //▲で呼ばれるコマンド
